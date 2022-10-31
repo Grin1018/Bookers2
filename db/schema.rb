59f831783c65def7437f07172f8fee6bfdab0dbd
EC2 Default User <ec2-user@ip-172-31-24-196.ap-northeast-1.compute.internal>
2022-10-31 13:51:22 +0000

    forth push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_30_003559) do
+ActiveRecord::Schema.define(version: 2022_10_31_023742) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_10_30_003559) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "introduction"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
